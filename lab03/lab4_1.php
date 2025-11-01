622a30347df1dec6ec64b0f0044c6eb7b0aff5d1
anna123-mmm <annaminh2708@gmail.com>
2025-11-01 11:58:23 +0700

    Add files via upload

---
+++ lab03/lab4_1.php
@@ -0,0 +1,7 @@
+<?php
+$s1=0;
+for($i=2;$i<=100;$i+=2)
+    $s1+=$i;
+
+    echo "Tong cac so chan: $s1";
+?>
---
+++ lab03/lab4_2.php
@@ -0,0 +1,11 @@
+<?php
+$i=0;
+$s2=0;
+while($s2<1000)
+{
+    $i++;
+    $s2+=$i;
+}
+
+echo "So n nho nhat: $i";
+?>
